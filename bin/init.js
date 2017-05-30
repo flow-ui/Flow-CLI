16f96f7d805573156b338fb01aa7b7dac9ef602e
tower1229 <tower1229@qq.com>
2017-05-30 13:57:02 +0800

    3.6.4

--- bin/init.js
+++ bin/init.js
@@ -6,7 +6,7 @@ const init = function() {
 	const spinner = ora('正在下载模板');
 	spinner.color = 'yellow';
 	spinner.start();
-	download('tower1229/front-flow-template', '.', function(err) {
+	download('tower1229/Flow-Template', '.', function(err) {
 		if (err) return console.log(err);
 		spinner.text = '项目初始化完成！';
 		spinner.succeed();
